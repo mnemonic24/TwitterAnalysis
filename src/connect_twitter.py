36a6543086e8c91d81d4144cae0f068d705c3010
tomori <nt24101310@gmail.com>
2019-08-01 17:05:07 +0900

    make get screen_name method

--- src/connect_twitter.py
+++ src/connect_twitter.py
@@ -3,27 +3,35 @@
 from requests_oauthlib import OAuth1Session
 import json
 import sys
+import datetime
 
 
 class Twitter:
     def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
         self.api = OAuth1Session(consumer_key, consumer_secret, access_token, access_token_secret)
 
-    def get_tweet_users(self, word, count, url = "https://api.twitter.com/1.1/search/tweets.json"):
-        params ={'q': word, 'count': count}
+    def get_tweet_users(self, word, target='id', count=5, result_type='recent', 
+                        until=str(datetime.date.today()), url="https://api.twitter.com/1.1/search/tweets.json"):
+        
+        params ={'q': word, 'count': count, 'result_type': result_type, 'until': until}
+        user_list = []
+        
         req = self.api.get(url, params=params)
-        id_list = []
+        if target not in ['id', 'screen_name']:
+            print('Don\'t have {} param, use id or screen_name'.format(target))
+            return
         if req.status_code == 200:
             search_timeline = json.loads(req.text)
             for tweet in search_timeline['statuses']:
-                id_list.append(tweet['user']['id'])
-            id_list = list(set(id_list))
-            return id_list
+                user_list.append(tweet['user'][target])
+            user_list = list(set(user_list))
+            return user_list
         else:
             print("ERROR: %d" % req.status_code)
-            sys.exit()
+            return
 
-    def get_followers_id(self, scn, count=5, url='https://api.twitter.com/1.1/followers/list.json'):
+    # count <= 5000
+    def get_followers_id(self, id, count=5, url='https://api.twitter.com/1.1/followers/ids.json'):
         params = {'ids': id, 'cursor': -1, 'count': count}
         req = self.api.get(url, params=params)
         if req.status_code == 200:
@@ -31,8 +39,22 @@ class Twitter:
             return followers_list
         else: 
             print("ERROR: %d" % req.status_code)
-            sys.exit()
+            return None
 
+    # scn = screen name, count <= 200
+    def get_followers_scn(self, scn, count=5, url='https://api.twitter.com/1.1/followers/list.json'):
+        params = {'screen_name': scn, 'cursor': -1, 'count': count}
+        req = self.api.get(url, params=params)
+        if req.status_code == 200:
+            followers_list = []
+            for user in req.json()['users']:
+                followers_list.append(user['screen_name'])
+            return followers_list
+        else: 
+            print("ERROR: %d" % req.status_code)
+            return None
+
+    # count <= 5000
     def get_following_id(self, id, count=5, url='https://api.twitter.com/1.1/friends/ids.json'):
         params = {'ids': id, 'cursor': -1, 'count': count}
         req = self.api.get(url, params=params)
@@ -41,8 +63,17 @@ class Twitter:
             return following_list
         else: 
             print("ERROR: %d" % req.status_code)
-            return []
+            return None
 
-
-if __name__ == "__main__":
-    pass
+    # scn = screen name, count <= 200
+    def get_following_scn(self, scn, count=5, url='https://api.twitter.com/1.1/friends/list.json'):
+        params = {'screen_name': scn, 'cursor': -1, 'count': count}
+        req = self.api.get(url, params=params)
+        if req.status_code == 200:
+            following_list = []
+            for user in req.json()['users']:
+                following_list.append(user['screen_name'])
+            return following_list
+        else: 
+            print("ERROR: %d" % req.status_code)
+            return None
--- src/main.py
+++ src/main.py
@@ -9,16 +9,19 @@ import networkx as nx
 
 if __name__ == "__main__":
     twitter = Twitter(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-    tweet_id_list = twitter.get_tweet_users(word='#参院選2019', count=100)
+    tweet_scn_list = twitter.get_tweet_users(word='#参院選2019 -RT', count=30, 
+                                            result_type='popular', target='screen_name')
     data_dict = {}
-    if tweet_id_list != []:
-        for id in tqdm(tweet_id_list):
-            following_list = twitter.get_following_id(id=id, count=1000)
-            if following_list != []:
+    if tweet_scn_list is not None:
+        for scn in tqdm(tweet_scn_list):
+            following_list = twitter.get_following_scn(scn=scn, count=200)
+            if following_list is not None:
                 data_dict[id] = following_list
+                sleep(60) # because Twitter API block 15req / 15m  
             else: 
                 break
-            sleep(60) # because Twitter API block 15req / 15m  
 
-    graph = nx.from_dict_of_lists(data_dict)
-    nx.write_gml(graph, 'data/twitter.gml')
+    # dict change to graph and save graph
+    if data_dict != {}:
+        graph = nx.from_dict_of_lists(data_dict)
+        nx.write_gml(graph, 'data/twitter.gml')
